067782cab9c610f4a96a082a2911a846e0b35ad8
KeresztesHunor <kereszteshunor36@gmail.com>
2025-04-22 17:06:49 +0200

    Program.ReadRecipesFile -> Form1.ReadRecipesFile

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,29 @@
+using System.Text.Json;
+
 namespace FactorioRateCalculator
 {
     public partial class Form1 : Form
     {
         IReadOnlyList<Recipe> recipes { get; }
 
-        public Form1(IReadOnlyList<Recipe> recipes)
+        public Form1()
         {
             InitializeComponent();
-            this.recipes = recipes;
+            this.recipes = ReadRecipesFile();
+        }
+
+        static IReadOnlyList<Recipe> ReadRecipesFile()
+        {
+            using FileStream fs = new FileStream("../../../recipe.json", FileMode.Open, FileAccess.Read);
+            return JsonSerializer.Deserialize<List<Recipe>>(fs, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
+                PropertyNameCaseInsensitive = true,
+                Converters = {
+                    new EnumTypeJsonConverter<ProductType>(Utils.KebabToPascalCase, Utils.PascalToKebabCase),
+                    new EnumTypeJsonConverter<ProductionCategory>(Utils.KebabToPascalCase, Utils.PascalToKebabCase)
+                }
+            })!;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,20 +13,7 @@ namespace FactorioRateCalculator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1(ReadRecipesFile()));
-        }
-
-        static IReadOnlyList<Recipe> ReadRecipesFile()
-        {
-            using FileStream fs = new FileStream("../../../recipe.json", FileMode.Open, FileAccess.Read);
-            return JsonSerializer.Deserialize<List<Recipe>>(fs, new JsonSerializerOptions {
-                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
-                PropertyNameCaseInsensitive = true,
-                Converters = {
-                    new EnumTypeJsonConverter<ProductType>(Utils.KebabToPascalCase, Utils.PascalToKebabCase),
-                    new EnumTypeJsonConverter<ProductionCategory>(Utils.KebabToPascalCase, Utils.PascalToKebabCase)
-                }
-            })!;
+            Application.Run(new Form1());
         }
     }
 }
